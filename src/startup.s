19493f623e40b1d97dee8f2e00cdecb7971d926f
Martin Rechsteiner <mrechsteiner@fastmail.com>
2025-08-18 18:06:40 +0200

    Add basic system handlers to vector table

--- src/startup.s
+++ src/startup.s
@@ -22,39 +22,63 @@
 // The .global directive is used to declare symbols that should be
 // visible outside the object file. We define two symbols, one for the
 // vector interrupt table and the reset handler.
-.global _vector_table
-.global _reset_handler
+.global vector_table
+.global reset_handler
 
-.include "constants.s"
+.type vector_table, %object
 
-_vector_table:
+vector_table:
         // The _estack value comes from the linker script and denotes
         // the position of the end the stack. Placing it as the first
         // word in the vector table means that it's used as the
         // initial stack pointer when the micro controller is powered
         // on or reset.
         .word _estack
+        
         // Next we place the address of the reset_handler label. In
         // ARM Cortex-M microcontrollers, after the initial stack
         // pointer value, the next entry in the vector table is the
         // start address of the reset handler, which is the code that
         // runs immediately after a reset.
-        .word _reset_handler
+        .word reset_handler
 
-// Setting .thumb_func here is needed in order for this to run correctly.
-// See: http://svenssonjoel.github.io/pages-2021/cortex-m-assembler-1-hardfault/index.html
-.thumb_func
-_reset_handler:
-        // Load the address of _estack into r0. The = before _estack
-        // tells the assembler to load the address and not the value.
-        ldr r0, =_estack
-        
+        // The following entries are the standard ARM Cortex-M
+        // exception handlers. Most point to default_handler which
+        // provides a simple infinite loop for debugging. This is not
+        // a complete list of handlers, just enough to define the
+        // systick_handler for handling delay timers.
+        .word default_handler   // Non maskable interrupt
+        .word default_handler   // All class of faults
+        .word default_handler   // Memory management
+        .word default_handler   // Pre-fetch fault, memory access fault
+        .word default_handler   // Undefined instruction or illegal state
+        .word 0                 // Reserved
+        .word 0                 // Reserved
+        .word 0                 // Reserved
+        .word 0                 // Reserved
+        .word default_handler   // System service call via SWI instruction
+        .word default_handler   // Debug Monitor
+        .word 0                 // Reserved
+        .word default_handler   // Pendable request for system service
+        .word default_handler   // System tick timer
+
+.type reset_handler, %function
+
+reset_handler:
         // The sp register is the stack pointer, which keeps track of
         // the top of the stack. By setting sp to _estack, this code
         // initializes the stack pointer to the end/top of the stack,
         // ensuring that the program has a valid stack to work with
         // from the start.
-        mov sp, r0
-        
+        ldr sp, =_estack
+
         // Branch to the main program
         b main
+
+.type default_handler, %function
+
+// The default handler is used for unused system handlers and
+// interrupts. Just goes into an infinite loop.
+default_handler:
+        b default_handler
+
