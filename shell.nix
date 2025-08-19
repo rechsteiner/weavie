87e4820445eb964be5c51773edd69d60bbd7bbd8
Martin Rechsteiner <mrechsteiner@fastmail.com>
2025-08-19 19:10:28 +0200

    Remove Rust from nix-shell

--- shell.nix
+++ shell.nix
@@ -5,21 +5,8 @@ mkShell {
     qemu
     gcc-arm-embedded
     dfu-util
-
     cmake
     libiconv
-    rustc
-    cargo
-    rust-analyzer
-    darwin.apple_sdk.frameworks.Security
-    darwin.apple_sdk.frameworks.CoreFoundation
-    darwin.apple_sdk.frameworks.CoreServices
-    darwin.apple_sdk.frameworks.Foundation
-    darwin.apple_sdk.frameworks.ForceFeedback
-    darwin.apple_sdk.frameworks.AVFoundation
-    darwin.apple_sdk.frameworks.AppKit
-    darwin.apple_sdk.frameworks.Cocoa
-    darwin.apple_sdk.frameworks.MetalKit
   ];
   nativeBuildInputs = with pkgs; [
     pkg-config
